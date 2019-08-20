1ab0b6a9e32f450e69306b29f2189e3d3a7bf343
Felipe Perez <felipepjaramillo@gmail.com>
2019-08-20 14:09:48 -0500

    controlador 1ra parte

--- src/Controlador/ControlBanco.java
+++ src/Controlador/ControlBanco.java
@@ -1,12 +1,91 @@
 package Controlador;
 
-import Vista.PanelTitulo;
-import Vista.VentanaPrincipal;
+import Vista.*;
+import Modelo.*;
 
-public class ControlBanco {
-VentanaPrincipal vp;
+public class ControlBanco 
+{
+	VentanaPrincipal vp;
+	Banco banco;
+	Cliente cliente;
+	Antecedente antecedente;
 
-public ControlBanco() {
-	vp= new VentanaPrincipal();
-}
+	public ControlBanco()
+	{
+		vp= new VentanaPrincipal();
+		banco= new Banco();
+	}
+
+	public Cliente buscarCliente(String pCedula)
+	{
+		Cliente c = null;
+		for(int i=0; i<banco.getClientes().size(); i++)
+		{
+			if(c.getCedula() == pCedula)
+			{
+				c=banco.getClientes().get(i);
+			}
+		}
+		return c;
+	}
+	
+	public Antecedente buscarAntecedente(int pCodigo)
+	{
+		Antecedente a = null;
+		for(int i=0; i<cliente.getAntecedentes().size(); i++)
+		{
+			if(a.getCodigo() == pCodigo)
+			{
+				a=cliente.getAntecedentes().get(i);
+			}
+		}
+		return a;
+	}
+	
+	public void agregarCliente(String pNombre, String pCedula, int pEdad, char pGenero)
+	{
+		Cliente c = buscarCliente(pCedula);
+		if(c==null)
+		{
+			c = new Cliente(pNombre, pCedula, pEdad, pGenero);
+			banco.getClientes().add(c);
+		}
+	}
+	
+	public void agregarAntecedente(int pCodigo, String pInformacion)
+	{
+		Antecedente a = buscarAntecedente(pCodigo);
+		if(a==null)
+		{
+			a = new Antecedente(pCodigo, pInformacion);
+			cliente.getAntecedentes().add(a);
+		}
+	}
+	
+	public void eliminarCliente(String pCedula)
+	{
+		Cliente c = buscarCliente(pCedula);
+		if(c!=null)
+		{
+			banco.getClientes().remove(c);
+		}
+	}
+	
+	public void eliminarAntecedente(int pCodigo)
+	{
+		Antecedente a = buscarAntecedente(pCodigo);
+		if(a!=null)
+		{
+			cliente.getAntecedentes().remove(a);
+		}
+	}
+	
+	public void modificarCliente(String pCedula)
+	{
+		Cliente c = buscarCliente(pCedula);
+		if(c!=null)
+		{
+			
+		}
+	}
 }
--- src/Modelo/Antecedente.java
+++ src/Modelo/Antecedente.java
@@ -2,4 +2,30 @@ package Modelo;
 
 public class Antecedente {
 
+	private int codigo;
+	private String descripcion;
+	
+	public Antecedente(int pCodigo, String pDescripcion)
+	{
+		codigo = pCodigo;
+		descripcion = pDescripcion;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
 }
