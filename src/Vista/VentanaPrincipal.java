5dd52e1ccccc7ef22d7c9f31ac9c5708fbfaf1c5
Bryan <sanbryan11@gmail.com>
2019-08-26 17:00:21 -0500

    PANEL LISTA

--- src/Vista/VentanaPrincipal.java
+++ src/Vista/VentanaPrincipal.java
@@ -1,25 +1,36 @@
 package Vista;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
 public class VentanaPrincipal extends JFrame {
 	PanelTitulo pt;
-
+	PanelClientes pc;
+	
+	
 	public VentanaPrincipal() {
 		setTitle("BANCO TOBEPE " + "¡ DONDE TU DINERO ESTA SEGURO !");
-		setSize(Toolkit.getDefaultToolkit().getScreenSize().width,Toolkit.getDefaultToolkit().getScreenSize().height);
-
 		setResizable(false);
-		setLayout(new BorderLayout());
+		setLayout(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
+		this.getContentPane().setBackground(Color.white);
 		pt = new PanelTitulo();
-		add(pt);
+		pc = new PanelClientes();
+		pt.setBackground(Color.white);
+		pc.setBackground(Color.white);
+		getContentPane().add(pt).setBounds(0, 0, 1385, 50);
+		getContentPane().add(pc).setBounds(0, 50,432, 680);;
 		setVisible(true);
+		
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
 
+	
 	}
 
 }
