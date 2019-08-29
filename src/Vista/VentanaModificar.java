69b60ecc0e5f3fc1c8b860980216f7002bff2cfc
Bryan <sanbryan11@gmail.com>
2019-08-29 16:12:42 -0500

    Interfaz grafica

---
+++ src/Vista/VentanaModificar.java
@@ -0,0 +1,122 @@
+package Vista;
+
+import java.awt.GridLayout;
+
+import javax.swing.*;
+
+public class VentanaModificar extends JFrame {
+	JLabel nombre;
+	JLabel cedula;
+	JLabel edad;
+	JLabel genero;
+
+	JTextField nombre_;
+	JTextField cedula_;
+	JTextField edad_;
+	JTextField genero_;
+
+	JButton modificar;
+
+	public VentanaModificar() {
+		setTitle("MODIFICAR ESTUDIANTE");
+		setSize(700, 620);
+		setResizable(false);
+		setLayout(new GridLayout(4, 1));
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setLocationRelativeTo(null);
+
+		nombre = new JLabel("NOMBRE:");
+		add(nombre);
+		nombre_ = new JTextField(30);
+		add(nombre_);
+		cedula = new JLabel("CEDULA:");
+		add(cedula);
+		cedula_ = new JTextField(30);
+		add(cedula_);
+		edad = new JLabel("EDAD:");
+		add(edad);
+		edad_ = new JTextField(30);
+		add(edad_);
+		genero = new JLabel("GENERO");
+		add(genero);
+		genero_ = new JTextField(30);
+		add(genero_);
+		modificar = new JButton("MODIFICAR");
+		add(modificar);
+		modificar.setActionCommand("MODIFICAR");
+
+	}
+
+	public JButton getModificar() {
+		return modificar;
+	}
+
+	public void setModificar(JButton modificar) {
+		this.modificar = modificar;
+	}
+
+	public JLabel getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(JLabel nombre) {
+		this.nombre = nombre;
+	}
+
+	public JLabel getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(JLabel cedula) {
+		this.cedula = cedula;
+	}
+
+	public JLabel getEdad() {
+		return edad;
+	}
+
+	public void setEdad(JLabel edad) {
+		this.edad = edad;
+	}
+
+	public JLabel getGenero() {
+		return genero;
+	}
+
+	public void setGenero(JLabel genero) {
+		this.genero = genero;
+	}
+
+	public JTextField getNombre_() {
+		return nombre_;
+	}
+
+	public void setNombre_(JTextField nombre_) {
+		this.nombre_ = nombre_;
+	}
+
+	public JTextField getCedula_() {
+		return cedula_;
+	}
+
+	public void setCedula_(JTextField cedula_) {
+		this.cedula_ = cedula_;
+	}
+
+	public JTextField getEdad_() {
+		return edad_;
+	}
+
+	public void setEdad_(JTextField edad_) {
+		this.edad_ = edad_;
+	}
+
+	public JTextField getGenero_() {
+		return genero_;
+	}
+
+	public void setGenero_(JTextField genero_) {
+		this.genero_ = genero_;
+	}
+
+}
